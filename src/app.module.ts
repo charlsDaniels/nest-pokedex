151aecea0f8c2438cfad6de3e459d9bd979683bd
Carlos Menendez <cdmenendez@teco.com.ar>
2023-11-08 09:45:10 -0300

    Fin seccion 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public')
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
-    SeedModule
-    
+    SeedModule,
+    CommonModule
   ]
 })
 export class AppModule {}
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+import { IsString, IsInt, IsPositive, Min, MinLength } from "class-validator"
+
+export class CreatePokemonDto {
+  
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number
+
+  @IsString({ message: 'name debe ser un string'})
+  @MinLength(1)
+  name: string
+  
+}
